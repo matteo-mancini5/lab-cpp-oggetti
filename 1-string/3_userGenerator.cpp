40e457de1eebd22e100e525147b38ccca8d08a19
matteo-mancini5 <mancini.matteo2008@gmail.com>
2025-12-13 11:23:34 +0100

    Update 3_userGenerator.cpp

--- 1-string/3_userGenerator.cpp
+++ 1-string/3_userGenerator.cpp
@@ -1,61 +1,47 @@
-/***************************************************************************
- *                         Username generator                              *
- *                                                                         *
- *  Capire il funzionamento della funzione minuscolo                       *
- *  e implementare le due funzioni usernameGenerator                       *
- **************************************************************************/
-
- #include <iostream>
+#include <iostream>
 #include <string>
 #include <cctype>
 
 using namespace std;
 
-
-// Funzione che trasforma una stringa in minuscolo
 string minuscolo(string s){
     for(int i = 0; i < s.length(); i++)
         s.at(i) = tolower(s.at(i));
     return s;
 }
 
-/*
-    TODO: Implementare la funzione affinchè, a partire da 2 stringhe nome e cognome
-          generi in output una stringa contenente uno username utente nella forma:
-          prima lettera del nome + tutto il cognome (tutto minuscolo)
-*/
 string usernameGenerator(string nome, string cognome){
-    return "da implementare";
+    string username;
+    username += nome.at(0);
+    username += cognome;
+    return minuscolo(username);
 }
 
-/*
-    TODO: Implementare la funzione affinchè, a partire da 1 stringa nella forma "Nome Cognome"
-          generi in output una stringa contenente uno username utente nella forma:
-          prima lettera del nome + tutto il cognome (tutto minuscolo)
-    
-    Hint: rispetto alla funzione precedente, splittare (spezzare il nome dal cognome, basandosi sullo spazio)
-          e ripetere la stessa logica. E' possibile anche evitare di ripetere la logica? Magari chiamando la stessa funzione sopra?
-
-*/
 string usernameGenerator(string nomeCognome){
-    return "da implementare";
+    int spazio = nomeCognome.find(' ');
+
+    string nome = nomeCognome.substr(0, spazio);
+    string cognome = nomeCognome.substr(spazio + 0);
+
+    return usernameGenerator(nome, cognome);
 }
 
-// main con testing
+
 int main()
 {
-
     string nome = "Mario";
     string cognome = "Rossi";
 
     string nomeCognome = "Luca Bianchi";
 
     string username1 = usernameGenerator(nome, cognome);
-
     string username2 = usernameGenerator(nomeCognome);
 
-    cout << "Primo utente: " << nome << " " << cognome << ". Username: " << username1;
-    cout << "Secondo utente: " << nomeCognome << ". Username: " << username2;
+    cout << "Primo utente: " << nome << " " << cognome
+         << ". Username: " << username1 << endl;
+
+    cout << "Secondo utente: " << nomeCognome
+         << ". Username: " << username2 << endl;
 
     return 0;
 }
